5e6201da8853ea942dcbb846d0a6bbc30afbcb62
3ygn <dsoller@outlook.com>
2015-11-15 01:19:01 -0500

    Made the navbar bigger and with no padding

--- app/build/generated/source/r/debug/com/example/esi/R.java
+++ app/build/generated/source/r/debug/com/example/esi/R.java
@@ -19,13 +19,13 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int LinearLayout1=0x7f080004;
+        public static final int LinearLayout1=0x7f080000;
         public static final int buttonAddToDo=0x7f080008;
         public static final int checkToDoItem=0x7f08000a;
-        public static final int imageButton1=0x7f080000;
-        public static final int imageButton2=0x7f080001;
-        public static final int imageButton3=0x7f080002;
-        public static final int imageButton4=0x7f080003;
+        public static final int imageButton1=0x7f080001;
+        public static final int imageButton2=0x7f080002;
+        public static final int imageButton3=0x7f080003;
+        public static final int imageButton4=0x7f080004;
         public static final int listViewToDo=0x7f080009;
         public static final int loadingProgressBar=0x7f080006;
         public static final int menu_refresh=0x7f08000b;
